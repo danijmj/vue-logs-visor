c9a7b9efccf2dc46faf14ed7e925b4d1ae646d7e
danijmj <danielmorales@gnoss.com>
2024-02-07 10:58:05 +0100

    Changes in the app vue for the new upload file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Logs de AbsysNet" src="./assets/e-log.png" width="300px">
+  <CheckItems msg="Visor de los archivos obtenidos, y su estado, en AbsysNet"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckItems from './components/CheckItems.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CheckItems
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: var(--bs-body-font-family);
+  font-size: var(--bs-body-font-size);
+  font-weight: var(--bs-body-font-weight);
+  line-height: var(--bs-body-line-height);
+  color: var(--bs-body-color);
+  text-align: var(--bs-body-text-align);
+  background-color: var(--bs-body-bg);
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: transparent;
+  background-color: #f4f6fc;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
